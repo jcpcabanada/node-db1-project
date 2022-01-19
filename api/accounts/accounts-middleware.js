f36b483cb425b71795f8d2645d29c1ec543aa297
jcpcabanada <jcpcabanada@gmail.com>
2022-01-19 03:22:11 -0800

    10th

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,24 +2,23 @@ const Account = require('./accounts-model')
 const db = require('../../data/db-config');
 
 exports.checkAccountPayload = (req, res, next) => {
-  const error = { status: 400 }
-  const { name, budget } = req.body
-  if (name === undefined || budget === undefined) {
-    error.message = 'name and budget are required'
-  } else if (typeof name !== 'string') {
-    error.message = 'name of account must be a string'
-  } else if (name.trim().length < 3 || name.trim().length > 100) {
-    error.message = 'name of account must be between 3 and 100'
-  } else if (typeof budget !== 'number' || isNaN(budget)) {
-    error.message = 'budget of account must be a number'
-  } else if (budget < 0 || budget > 1000000) {
-    error.message = ' budget of account is too larger or too small'
-  }
-  if (error.message) {
-    next(error)
-  } else {
-    next()
-  }
+  const error = {status: 400}
+    const {name, budget} = req.body
+
+    if (name === undefined || budget === undefined) {
+        error.message = 'name and budget are required'
+    } else if (name.trim().length < 3 || name.trim().length > 100) {
+        error.message = 'name of account must be between 3 and 100'
+    } else if (typeof budget !== 'number' || isNaN(budget)) {
+        error.message = 'budget of account must be a number'
+    } else if (budget < 0 || budget > 100000) {
+        error.message = 'budget of account is too large or too small'
+    }
+    if (error.message) {
+        next(error)
+    } else {
+        next()
+    }
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
@@ -41,7 +40,7 @@ exports.checkAccountId = async (req, res, next) => {
   try {
     const account = await Account.getById(req.params.id)
     if (!account) {
-      next({ status: 404, message: 'not found' })
+      next({ status: 404, message: 'account not found' })
     } else {
       req.account = account
       next()
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const res = require('express/lib/response')
 const accountsMidware = require('./accounts-middleware')
 const Account = require('./accounts-model')
 
@@ -14,41 +15,46 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get(
-    '/:id',
-    accountsMidware.checkAccountId,
-    async(req, res, next) => {
-  try {
-    const account = await Account.getById(req.params.id)
-    res.json(req.account)
-  } catch (err) {
-    next(err)
-  }
-})
+  '/:id',
+  accountsMidware.checkAccountId,
+  async (req, res, next) => {
+    try {
+      const account = await Account.getById(req.params.id)
+      res.json(req.account)
+    } catch (err) {
+      next(err)
+    }
+  })
 
 router.post(
-    '/',
-    accountsMidware.checkAccountPayload,
-    accountsMidware.checkAccountNameUnique,
-    async (req, res, next) => {
-  try {
-    const newAccount = await Account.create(req.body)
-    res.status(201).json(newAccount)
-  } catch (err) {
-    next(err)
-  }
-})
+  '/',
+  accountsMidware.checkAccountPayload,
+  accountsMidware.checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const newAccount = await Account.create({
+        name:req.body.name.trim(),
+        budget: req.body.budget,
+      })
+      res.status(201).json(newAccount)
+    } catch (err) {
+      next(err)
+    }
+  })
 
 router.put(
-    '/:id',
-    accountsMidware.checkAccountPayload,
-    accountsMidware.checkAccountNameUnique,
-    accountsMidware.checkAccountId,
-    (req, res, next) => {
-  try {
-    res.json('put accounts')
-  } catch (err) {
-    next(err)
-  }
+  '/:id',
+  accountsMidware.checkAccountPayload,
+  accountsMidware.checkAccountNameUnique,
+  accountsMidware.checkAccountId,
+  async (req, res, next) => {
+    const updated = await Account.updateById(req.params.id, req.body)
+res.json(updated)
+try {
+  res.json('update account')
+} catch (err) {
+  next(err)
+}
 });
 
 router.delete('/:id', accountsMidware.checkAccountId, async (req, res, next) => {
@@ -61,8 +67,8 @@ router.delete('/:id', accountsMidware.checkAccountId, async (req, res, next) => 
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  res.status(err.status || 500 ).json({
-    message:err.message
+  res.status(err.status || 500).json({
+    message: err.message
   })
 })
 
